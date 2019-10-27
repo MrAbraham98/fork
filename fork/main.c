f8eb46776d9c3e34f79e88b959f93c241a11835f
MrAbraham98 <i_bucakli1998>
2019-10-28 02:28:25 +0300

    Initial Commit

---
+++ fork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fork
+//
+//  Created by ibrahim bucaklı on 28.10.2019.
+//  Copyright © 2019 ibrahim bucaklı. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

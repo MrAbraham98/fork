d47578c4d6dcc48c72710667297e126ac7728bd0
MrAbraham98 <i_bucakli1998>
2019-10-28 02:32:27 +0300

    Initial Commit

--- fork/main.c
+++ fork/main.c
@@ -7,9 +7,45 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void parentProcess(void);
+int childProcess(int sayi);
+
+int main(void)
+{
+    int sayi;
+    pid_t pid;
+    printf("Pozitif bir sayı giriniz \n");
+    scanf("%d", &sayi);
+    pid =fork();
+    if(pid == 0){
+        childProcess(sayi);
+    }
+   else{
+        wait(NULL);
+        parentProcess();
+    }
+}
+int childProcess(int sayi){
+    while (1) {
+        int mod = sayi % 2;
+        if(mod == 0){
+            sayi = sayi / 2;
+            printf("Child Process : %d\n",sayi);
+        }
+        if(mod == 1){
+            sayi = sayi * 3 + 1;
+            printf("Child Process : %d\n",sayi);
+        }
+        if(sayi == 1){
+            break;
+        }
+    }
     return 0;
+}
+void parentProcess(void){
+    printf("Parent process\n");
 }
